ee12b1d2d8ed3ac5582f9e915368d7fcc441307a
badriverma <badriverma135@gmail.com>
2025-01-23 00:04:49 +0530

    find the index of two sum numbers

--- app_java_program/src/array_programs/IndexOfSumNumbers.java
+++ app_java_program/src/array_programs/IndexOfSumNumbers.java
@@ -2,4 +2,23 @@ package array_programs;
 
 public class IndexOfSumNumbers {
 
+	public static void main(String[] args) {
+		int[] arr= {2,8,7,9,6};
+		int target = 9;
+		
+		int[] findIndex = findIndexOfTwoSumNumbers(arr,target);
+		for (int i : findIndex) {
+			System.out.print(i+" ");
+		}
+		
+	}
+
+	private static int[] findIndexOfTwoSumNumbers(int[] arr, int target) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				return new int[] {i,j};
+			}
+		}
+		return null;
+	}
 }
