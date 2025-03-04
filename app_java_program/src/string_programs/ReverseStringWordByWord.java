a0f84f252e7d9948e863ec76ad5d23fb76580fcd
badriverma <badriverma135@gmail.com>
2025-03-04 23:49:20 +0530

    Reversed the given string word by word

---
+++ app_java_program/src/string_programs/ReverseStringWordByWord.java
@@ -0,0 +1,30 @@
+package string_programs;
+
+public class ReverseStringWordByWord {
+
+	public static void main(String[] args) {
+		String str= "I Love My Country";
+		String reversed = reverseStringWordByWord(str);
+		System.out.println(reversed.toString());
+	}
+
+	private static String reverseStringWordByWord(String str) {
+
+		StringBuilder reversedString = new StringBuilder();
+		StringBuilder currentString = new StringBuilder();
+		
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch==' ') {
+				reversedString.insert(0, currentString).insert(0, ' ');
+				currentString.setLength(0);
+			}else {
+				currentString.append(ch);
+			}
+		}
+		
+		reversedString.insert(0, currentString);
+		
+		return reversedString.toString();
+	}
+}
