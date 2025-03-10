d619e4afecd0dab786f12921d2d2fe0b3e399884
badriverma <badriverma135@gmail.com>
2025-03-10 22:57:07 +0530

    find the occurence of given number

--- app_java_program/src/string_programs/FindTheOccurenceOfGivenString.java
+++ app_java_program/src/string_programs/FindTheOccurenceOfGivenString.java
@@ -1,7 +1,9 @@
 package string_programs;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -10,15 +12,25 @@ public class FindTheOccurenceOfGivenString {
 	public static void main(String[] args) {
 		String str = "badriprasadverma";
 		
-		//type 1
+		//Type 1
 		Map<Character, Long> collect = str.chars().mapToObj(c-> (char)c)
 		.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
 		System.out.println(collect);
 		
-		//type2
+		//Type2
 		Map<String, Long> collect2 = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(),
 				Collectors.counting()));
 		System.out.println(collect2);
-
+		
+		//Type 3
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for (char ch : str.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0)+1);
+		}
+		
+		for (Entry<Character, Integer> string : map.entrySet()) {
+			System.out.println(string);
+		}
 	}
 }
